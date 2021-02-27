bdbe2750755e211d69c1c3e591867bec114070a8
Bichurina Anastasiia <69593629+bratzzie@users.noreply.github.com>
2021-02-27 23:05:55 +0200

    changed cors & problems with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,31 +1,47 @@
+import useFetchJobs from "./hooks/useFetchJobs";
+import { Container } from "react-bootstrap";
+import { useState } from "react";
 
-import './App.css';
-import useFetchJobs from './hooks/useFetchJobs';
-import { Container } from 'react-bootstrap';
-import { useState } from 'react';
-import Job from './components/Job';
-import OffersPagination from './components/OffersPagination';
+import Job from "./components/Job";
+import OffersPagination from "./components/OffersPagination";
+import SearchBar from "./components/SearchBar";
 
 function App() {
-  const [params, setParams] = useState({})
-  const [page, setPage] = useState(1)
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
 
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page);
 
-  const {jobs, loading, error, hasNextPage} = useFetchJobs(params, page)
-  
+  function handleParamChange(e) {
+    const param = e.target.name;
+    const value = e.target.value;
+
+    setPage(1);
+
+    setParams((prevParams) => {
+      return { ...prevParams, [param]: value };
+    });
+  }
   return (
-   <Container className="my-4">
-     <h1 className="mb-4">GitHub Jobs</h1>
-     <OffersPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
-     {loading && <h1>Loading...</h1>}
-     {error && <h1>Something went wrong...Try Refreshing</h1>}
-     {jobs.map(job => {
-       return <Job key={job.id} job={job} />
-     })}
-     <OffersPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
-    
-
-   </Container>
+    <Container className="my-4">
+      <h1 className="mb-4">GitHub Jobs</h1>
+      <SearchBar params={params} onParamChange={handleParamChange} />
+      <OffersPagination
+        page={page}
+        setPage={setPage}
+        hasNextPage={hasNextPage}
+      />
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Something went wrong...Try Refreshing</h1>}
+      {jobs.map((job) => {
+        return <Job key={job.id} job={job} />;
+      })}
+      <OffersPagination
+        page={page}
+        setPage={setPage}
+        hasNextPage={hasNextPage}
+      />
+    </Container>
   );
 }
 
